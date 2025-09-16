47ef3fd8fa67e2e4a815c7980fa7f5770ef0491d
Paulo de Tarso Teles Ramos <pauloramos@kedu.com.br>
2025-09-16 18:27:37 -0300

    Add project files.

---
+++ EnvironmentMonitoringSystem.API/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EnvironmentMonitoringSystem.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Environment Monitoring System API is running.");
+        }
+    }
+}
