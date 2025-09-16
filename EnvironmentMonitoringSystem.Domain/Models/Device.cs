2af72fe6cc35004ec9df012220355d425287a8dd
Paulo de Tarso Teles Ramos <pauloramos@kedu.com.br>
2025-09-16 19:48:27 -0300

    Add project domain and models

---
+++ EnvironmentMonitoringSystem.Domain/Models/Device.cs
@@ -0,0 +1,12 @@
+﻿namespace EnvironmentMonitoringSystem.Domain.Models
+{
+    public class Device
+    {
+        public Guid Id { get; set; }
+        public string DeviceName { get; set; } = string.Empty;
+        public string Location { get; set; } = string.Empty;
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+        public DateTime? LastUpdate { get; set; }
+        public DateTime? DeletionDate { get; set; }
+    }
+}
---
+++ EnvironmentMonitoringSystem.Domain/Models/Event.cs
@@ -0,0 +1,14 @@
+﻿namespace EnvironmentMonitoringSystem.Domain.Models
+{
+    public class Event
+    {
+        public Guid Id { get; set; }
+        public Guid DeviceId { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public decimal Temperature { get; set; }
+        public decimal Humidity { get; set; }
+        public bool IsAlarm { get; set; }
+
+        public Device? Device { get; set; }
+    }
+}
