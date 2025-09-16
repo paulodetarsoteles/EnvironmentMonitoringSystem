e61d391abbeefad49f2f0d2eaaada95892305a5a
Paulo de Tarso Teles Ramos <pauloramos@kedu.com.br>
2025-09-16 19:48:53 -0300

    Add controllers

---
+++ EnvironmentMonitoringSystem.API/Controllers/DevicesController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EnvironmentMonitoringSystem.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DevicesController : ControllerBase
+    {
+        /// <summary>Lista todos os dispositivos cadastrados</summary>
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Erro inesperado");
+            }
+        }
+
+        /// <summary>Obtém os detalhes de um dispositivo específico</summary>
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Erro inesperado");
+            }
+        }
+
+        /// <summary>: Registra um novo dispositivo no sistema</summary>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] string value)
+        {
+            try
+            {
+                //TODO Registrar um novo dispositivo no moq
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Erro inesperado");
+            }
+        }
+
+        /// <summary>Atualiza as informações de um dispositivo</summary>
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] string value)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Erro inesperado");
+            }
+        }
+
+        /// <summary>Remove um dispositivo do seu sistema</summary>
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Erro inesperado");
+            }
+        }
+    }
+}
---
+++ EnvironmentMonitoringSystem.API/Controllers/EventsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EnvironmentMonitoringSystem.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        /// <summary>Recebe os eventos dos dispositivos</summary>
+        [HttpPost]
+        public async Task<IActionResult> ReceiveEvent()
+        {
+            using var reader = new StreamReader(Request.Body);
+
+            var body = await reader.ReadToEndAsync();
+
+            try
+            {
+                var json = System.Text.Json.JsonDocument.Parse(body);
+
+                foreach (var property in json.RootElement.EnumerateObject())
+                {
+                    Console.WriteLine($"{property.Name}: {property.Value}");
+                }
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine($"Não é um JSON válido. {ex.Message}");
+            }
+
+            return Ok(new { message = "Payload recebido" });
+        }
+    }
+}
