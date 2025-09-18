b39dc36102096b8d157f6913f921b8fce35fc1be
Paulo de Tarso Teles Ramos <pauloramos@kedu.com.br>
2025-09-17 21:08:50 -0300

    Dependency injections

--- EnvironmentMonitoringSystem.API/Extensions/DependencyInjectionConfig.cs
+++ EnvironmentMonitoringSystem.API/Extensions/DependencyInjectionConfig.cs
@@ -1,4 +1,6 @@
-﻿using EnvironmentMonitoringSystem.Infrastructure.Repositories;
+﻿using EnvironmentMonitoringSystem.Application.Services;
+using EnvironmentMonitoringSystem.Application.Services.Interfaces;
+using EnvironmentMonitoringSystem.Infrastructure.Repositories;
 using EnvironmentMonitoringSystem.Infrastructure.Repositories.Interfaces;
 using EnvironmentMonitoringSystem.Infrastructure.Transactions;
 using EnvironmentMonitoringSystem.Infrastructure.Transactions.Interfaces;
@@ -9,9 +11,15 @@ namespace EnvironmentMonitoringSystem.API.Extensions
     {
         public static void AddDependencyInjectionConfig(this IServiceCollection services)
         {
+            services.AddHttpClient();
             services.AddScoped<IUnityOfWork, UnityOfWork>();
             services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
 
+            services.AddScoped<IDeviceRepository, DeviceRepository>();
+            services.AddScoped<IEventRepository, EventRepository>();
+
+            services.AddScoped<IDeviceService, DeviceService>();
+            services.AddScoped<IEventService, EventService>();
         }
     }
 }
