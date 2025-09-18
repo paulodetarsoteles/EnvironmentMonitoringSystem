759b7d22db9828a85d4c681225685e942cfc683c
Paulo de Tarso Teles Ramos <pauloramos@kedu.com.br>
2025-09-17 21:06:30 -0300

    Adjust contexto do factory

--- EnvironmentMonitoringSystem.Infrastructure/Contexts/AppDbContext.cs
+++ EnvironmentMonitoringSystem.Infrastructure/Contexts/AppDbContext.cs
@@ -1,6 +1,8 @@
 ﻿using EnvironmentMonitoringSystem.Domain.Models;
 using EnvironmentMonitoringSystem.Infrastructure.Configuration;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
 
 namespace EnvironmentMonitoringSystem.Infrastructure.Contexts
 {
@@ -19,6 +21,26 @@ namespace EnvironmentMonitoringSystem.Infrastructure.Contexts
             modelBuilder.ApplyConfiguration(new EventConfiguration());
 
             base.OnModelCreating(modelBuilder);
+        }
+    }
+
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            
+            if (string.IsNullOrEmpty(connectionString)) throw new InvalidOperationException("Connection string não encontrada.");
+
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseSqlite(connectionString);
+
+            return new AppDbContext(optionsBuilder.Options);
         }
     }
 }
