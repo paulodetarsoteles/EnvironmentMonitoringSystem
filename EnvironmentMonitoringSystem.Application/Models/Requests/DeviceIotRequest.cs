a46c580dcce0d98ea5cf7f0af3d41ad6b3d660bc
Paulo de Tarso Teles Ramos <pauloramos@kedu.com.br>
2025-09-17 21:08:06 -0300

    Request and responses Iot

---
+++ EnvironmentMonitoringSystem.Application/Models/Requests/DeviceIotRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace EnvironmentMonitoringSystem.Application.Models.Requests
+{
+    public class DeviceIotRequest
+    {
+        public string DeviceName { get; set; } = string.Empty;
+        public string Location { get; set; } = string.Empty;
+        public string CallbackUrl { get; set; } = string.Empty;
+    }
+}
---
+++ EnvironmentMonitoringSystem.Application/Models/Responses/DeviceIotResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace EnvironmentMonitoringSystem.Application.Models.Responses
+{
+    public class DeviceIotResponse
+    {
+        public Guid? IntegrationId { get; set; }
+    }
+}
